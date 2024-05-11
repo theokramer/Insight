c44394351fb019d0ad05c5182d3ebd94b2eb185e
Theo <kramer.theo@icloud.com>
2024-05-11 22:11:59 +0200

    Fixed Misplacement of the Boxes

--- Insight/Utilities.swift
+++ Insight/Utilities.swift
@@ -5,4 +5,22 @@
 //  Created by Theo Kramer on 11.05.24.
 //
 
-import Foundation
+import UIKit
+
+// Convert UIImageOrientation to CGImageOrientation for use in Vision analysis.
+extension CGImagePropertyOrientation {
+    init(_ uiImageOrientation: UIImage.Orientation) {
+        switch uiImageOrientation {
+        case .up: self = .up
+        case .down: self = .down
+        case .left: self = .left
+        case .right: self = .right
+        case .upMirrored: self = .upMirrored
+        case .downMirrored: self = .downMirrored
+        case .leftMirrored: self = .leftMirrored
+        case .rightMirrored: self = .rightMirrored
+        default: self = .up
+        }
+    }
+}
+
