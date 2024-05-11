a9ff42105c14076608c7e4656c642d6cd13720f3
Theo <kramer.theo@icloud.com>
2024-05-11 21:39:39 +0200

    Initial Commit

---
+++ Insight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Insight
+//
+//  Created by Theo Kramer on 11.05.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
